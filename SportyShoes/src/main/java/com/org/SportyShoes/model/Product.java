009ca076739ba277471e99dad8319f089306b471
Jamsine <jasmine.r.davies@gmail.com>
2022-10-07 12:51:22 +0100

    created Product model

---
+++ SportyShoes/src/main/java/com/org/SportyShoes/model/Product.java
@@ -0,0 +1,47 @@
+package com.org.SportyShoes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String product_name;
+    private double price;
+    private String category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
