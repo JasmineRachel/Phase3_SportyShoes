cc82f5ec11954cf1226ee29f0a82e20659446910
Jamsine <jasmine.r.davies@gmail.com>
2022-10-21 13:46:08 +0100

    added view to add product and got the post request working

--- SportyShoes/src/main/java/com/org/SportyShoes/controller/ProductController.java
+++ SportyShoes/src/main/java/com/org/SportyShoes/controller/ProductController.java
@@ -3,17 +3,28 @@ package com.org.SportyShoes.controller;
 import com.org.SportyShoes.model.Product;
 import com.org.SportyShoes.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@RestController // this returns the data, it does not return it on th view. Just creating a rest api here!
+//@Controller //use this!
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @PostMapping("/product")
-    Product newProduct(@RequestBody Product newProduct){
+    @PostMapping(path="/product")
+    Product newProduct(Product newProduct){
+        System.out.print(newProduct);
         return productRepository.save(newProduct);
+    }
+
+    @GetMapping("/all_products")
+    List<Product> getAllProducts(){
+        return productRepository.findAll();
     }
 }
